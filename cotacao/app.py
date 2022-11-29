1ca73f71aa34c596b8ff6207a61aad188ebfaed1
HitalloAzevedo <azevedohitallo@gmail.com>
2022-11-29 19:08:58 -0300

    update files
    upando os arquivos na pasta do clube, alteraçoes nas moedas

---
+++ cotacao/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from cotacao import pegar_cotacao, pegar_cotacao_btc
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+
+    usd = pegar_cotacao('usd')
+    eur = pegar_cotacao('eur')
+    gbp = pegar_cotacao('gbp')
+    btc = pegar_cotacao_btc()
+
+    return render_template('index.html', usd=usd, eur=eur, gbp=gbp, btc=btc)
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
