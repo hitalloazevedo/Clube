76d4bf2974356571ca53c3e466a18975e2e17c38
HitalloAzevedo <azevedohitallo@gmail.com>
2022-11-29 19:49:27 -0300

    simplificando

--- cotacao/app.py
+++ cotacao/app.py
@@ -1,5 +1,29 @@
 from flask import Flask, render_template
-from cotacao import pegar_cotacao, pegar_cotacao_btc
+import requests
+
+url = ' https://economia.awesomeapi.com.br/last/{}-BRL'
+
+
+def pegar_cotacao(moeda:str):
+    moeda = moeda.upper().strip()
+
+    response = requests.get(url.format(moeda))
+    response_json = response.json()
+
+    preco = str(response_json[f'{moeda}BRL']['bid'])
+    preco = f'{preco:.4}'.replace('.', ',')
+
+    
+    return preco
+
+
+def pegar_cotacao_btc():
+    response = requests.get(url.format('btc'))
+    response_json = response.json()
+    preco = str(response_json[f'BTCBRL']['bid'])
+    preco = preco.replace('.', ',')
+    
+    return preco
 
 
 app = Flask(__name__)
